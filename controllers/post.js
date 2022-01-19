fe605e06bd2104f6031ef297ba693e7e0d1702c3
Fabio Guidotti <guidotti.fabio@outlook.it>
2022-01-20 00:19:39 +0100

    prepare to merge refactor version - take 1

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,31 @@
-const Discord = require('discord.js');
-const { MongoClient } = require("mongodb");
 const mongodb = require("mongodb");
 const methodDB = require("../mongodb_controll.js");
 const jwt = require('jsonwebtoken');
-const { post } = require('../routes');
+
+exports.show = async (req, res, next) => {
+    const token = req.cookies['jwt'];
+    jwt.verify(token, process.env.JWT_SECRET, async function (err, decoded) {
+        if (!err) {
+            var loged = true
+            if (decoded.master == 1) {
+                mastr = true
+            }
+            var post = "";
+            if (req.params.id.length == 24) {
+                post = await get_Object(req.params.id);
+            }
+            res.render('post/post', { loged: loged, master: mastr, Title: post.Title, Img: post.Img, Tag: post.Tag, date: post['Date'], text: post['Text'], Famiglia: post.Famiglia });
+        } else {
+            var loged = false
+            var mastr = false
+            var post = "";
+            if (req.params.id.length == 24) {
+                post = await get_Object(req.params.id);
+            }
+            res.render('post/post', { loged: loged, master: mastr, Title: post.Title, Img: post.Img, Tag: post.Tag, date: post['Date'], text: post['Text'], Famiglia: post.Famiglia });
+        }
+    });
+};
 
 exports.Make_post = (req, res) => {
     const token = req.cookies['jwt'];
@@ -145,4 +167,15 @@ exports.Delete_mission_db = async (req, res) => {
             }
         }
     });
+}
+
+async function get_Object(id_obj) {
+    var on_sevice_db = await methodDB.open_db();
+    if (on_sevice_db != 1) {
+        methodDB.settab_db("Post_blog");
+        var cursor = methodDB.serachbyid(id_obj);
+    } else {
+        return 1;
+    }
+    return cursor;
 }
